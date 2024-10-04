f1b909770f4fd68611dfb5d8e97f2bf81f312ab0
Robert Devereux <robert@Roberts-MacBook-Pro-2018.local>
2024-10-04 21:45:52 +0100

    Correcting error on ALLOWED_HOSTS

--- main/settings.py
+++ main/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 
-ALLOWED_HOSTS = ['basic4-pvt7.onrender.com','localhost', '127.0.0.1]
+ALLOWED_HOSTS = ['basic4-pvt7.onrender.com','localhost', '127.0.0.1']
 
 
 # Application definition
